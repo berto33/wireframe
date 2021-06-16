9b1c0ad83bf55d799a1d8e3561894ba47f836aef
berto33 <meneghinialberto33@gmail.com>
2021-06-16 17:54:21 +0200

    update

--- src/app/mock-corsi.ts
+++ src/app/mock-corsi.ts
@@ -1,7 +1,7 @@
 import { Corso } from './Corso';
 
 export const CORSI: Corso[] = [
-    {titolo: "Corso base in Javascript", progresso: 100, scadenza: "31/01/2000"},
+    {titolo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam praesentium natus vero, quae quos culpa distinctio. Deserunt impedit optio consequuntur nisi, maiores vero soluta illo explicabo non reprehenderit reiciendis iure!", progresso: 100, scadenza: "31/01/2000"},
     {titolo: "Corso intermedio in Javascript", progresso: 75, scadenza: "31/02/2000"},
     {titolo: "Corso avanzato in Javascript", progresso: 25, scadenza: "31/03/2000"},
     {titolo: "Corso intermedio in Angular", progresso: 50, scadenza: "31/04/2000"},
