836c2c284115ed1b22590d1a162a1a2dee2767d0
berto33 <meneghinialberto33@gmail.com>
2021-06-30 17:56:47 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,21 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './components/layout/main/app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
-import { CartaCorsoComponent } from './components/views/carta-corso/carta-corso.component';
-import { CartaCertificatoComponent } from './components/views/carta-certificato/carta-certificato.component';
-import { LezioneComponent } from './components/views/lezione/lezione.component';
 
 import { PaginaDashboardComponent } from './components/routes/pagina-dashboard/pagina-dashboard.component';
 import { PaginaUtenteComponent } from './components/routes/pagina-utente/pagina-utente.component';
 import { PaginaCorsoComponent } from './components/routes/pagina-corso/pagina-corso.component';
 import { PaginaLezioneComponent } from './components/routes/pagina-lezione/pagina-lezione.component';
 
+import { CartaCorsoComponent } from './components/views/carta-corso/carta-corso.component';
+import { CartaCertificatoComponent } from './components/views/carta-certificato/carta-certificato.component';
+import { LezioneComponent } from './components/views/lezione/lezione.component';
 
-
-@NgModule ({
+@NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
